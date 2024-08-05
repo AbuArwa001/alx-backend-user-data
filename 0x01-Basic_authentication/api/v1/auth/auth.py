d56c735f3abef3ab75038216d5805b48a0ac47c0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-05 14:23:28 +0300

    Add doc string  and edit requre method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,11 +11,29 @@ class Auth:
     Class Auth for the API
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        # return f"{False} - {path} "
-        return False
+        """
+        Check if path require auth
+        """
+        if not path or not excluded_paths:
+            return True
+        if not path.endswith('/'):
+            path = path + '/'
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+        # if path in excluded_paths:
+        #     return False
+        # else:
+        return True
 
     def authorization_header(self, request=None) -> str:
+        """
+        Check for Authorizationn Header
+        """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Check if the current user
+        """
         return None
