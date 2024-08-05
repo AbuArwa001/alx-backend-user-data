db0a82397a3119404e59803e61293bff53825474
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-05 13:40:54 +0300

    add forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized_status() -> str:
       - This endpoint must raise a 401 error by using abort
     """
     abort(401)
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbiden_status() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - This endpoint must raise a 403 error by using abort
+    """
+    abort(403)
