27bb5c70678e149de4c958f2371319137e93bc23
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-08 12:07:40 +0300

    Session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Auth module for the Session Auth
+"""
+from api.v1.auth.auth import Auth
+from models.user import User
+import base64
+from typing import TypeVar
+
+
+class SessionAuth(Auth):
+    """
+    Child class for Auth
+    """
