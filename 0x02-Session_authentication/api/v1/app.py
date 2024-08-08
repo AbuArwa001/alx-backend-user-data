b31b6ec74f514128052985a2c23573d7b9cedfd6
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-08 15:08:44 +0300

    Add condition for session cookies

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -35,9 +35,12 @@ def before_request():
         return
     # Define the list of paths that do not require authentication
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
+    if not auth.authorization_header(request) and not auth.session_cookie(request):
+        return None, abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
