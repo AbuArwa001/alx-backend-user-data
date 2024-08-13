017a04800ff92387e7bfa2fb02072eea94d51ef8
AbuArwa001 <khalfanathman12@yahoo.com>
2024-08-13 15:55:21 +0300

    Make it a func to auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,12 +17,14 @@ def _hash_password(password: str) -> bytes:
     salt = gensalt()
     return hashpw(bytes, salt)
 
+
 def _generate_uuid() -> str:
-        """
-        The function should
-        return a string representation of a new UUID.
-        """
-        return str(uuid4())
+    """
+    The function should
+    return a string representation of a new UUID.
+    """
+    return str(uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
